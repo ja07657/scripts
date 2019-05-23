2b58eaf162f288429a6b10452d6714a9fda4be9e
Jason Amato <amato_jason@yahoo.com>
2019-05-23 16:04:38 -0400

    hi

--- ja_mountiso.sh
+++ ja_mountiso.sh
@@ -5,7 +5,7 @@ racadm vmdisconnect
 racadm closessn -a
 racadm remoteimage -d
 
-racadm remoteimage -c -u Dell -p Pass1234 -l //10.40.54.65/share/iso/Dell_13G_Oct2018_firmware.iso
+racadm remoteimage -c -l 10.40.196.211:/export/iso/13G_May2019.iso
 racadm remoteimage -s
 racadm set iDRAC.ServerBoot.FirstBootDevice VCD-DVD
 racadm set iDRAC.ServerBoot.BootOnce 1
@@ -16,4 +16,5 @@ racadm serveraction powercycle
 #nfs# racadm remoteimage -c -l 10.40.54.199:/export/nfs/iso/10.40.54.199/iso/Dell_13G_Feb2019b_firmware.iso
 #http# racadm remoteimage -c -l http://10.40.196.209/iso/Dell_13G_June2018_firmware.iso
 #http# racadm remoteimage -c -l http://10.40.54.199/iso/Dell_13G_Feb2019b_firmware.iso
+#racadm remoteimage -c -u Dell -p Pass1234 -l //10.40.54.65/share/iso/Dell_13G_Oct2018_firmware.iso
 #10.40.196.209 10.40.54.199
